1766a0895a6642ba1ec5ea348e9f9222655390fb
petertsun <petertsun@gmail.com>
2021-09-21 23:23:38 -0400

    add list py

---
+++ week_01_python/lists.py
@@ -0,0 +1,11 @@
+
+
+board = []
+num_rows = 5
+num_columns = 6
+for row in range(num_rows):
+    board.append([])
+    for column in range(num_columns):
+        board[row].append(0)
+
+print(board)
